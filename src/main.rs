70b71ff099e7a7c3e0069e8084b07205a2f86264
crimsonwrench <garon312@gmail.com>
2020-03-20 15:08:12 +0300

    Encode and decode input, add tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 mod primes;
 mod rsa;
 
-use rsa::Rsa;
-
-fn main() {
-    let rsa: Rsa = Rsa::new();
-    println!("{:?}", rsa.encode(String::from("Test")));
-}
+fn main() {}
--- src/rsa.rs
+++ src/rsa.rs
@@ -1,10 +1,10 @@
-use crate::primes::{Primes, MutuallyPrimes};
+use crate::primes::{MutuallyPrimes, Primes};
 use rand::seq::IteratorRandom;
 
 struct NumberE {
     current: usize,
     euler: usize,
-    d: usize
+    d: usize,
 }
 
 impl NumberE {
@@ -12,7 +12,7 @@ impl NumberE {
         NumberE {
             current: 1,
             euler,
-            d
+            d,
         }
     }
 }
@@ -35,7 +35,7 @@ impl Iterator for NumberE {
 pub struct Rsa {
     public_part: usize,
     private_part: usize,
-    n: usize
+    n: usize,
 }
 
 impl Rsa {
@@ -44,18 +44,40 @@ impl Rsa {
         Rsa {
             public_part: keys.0,
             private_part: keys.1,
-            n: keys.2
+            n: keys.2,
         }
     }
 
-    pub fn encode(&self, string: String) -> Vec<usize> {
-        // TODO: Encoding
-        vec![1, 2, 3]
+    pub fn encode(&self, string: &str) -> String {
+        string
+            .to_string()
+            .into_bytes()
+            .iter()
+            .map(|&u| mod_exp::mod_exp(u as usize, self.public_part, self.n).to_string())
+            .collect::<Vec<String>>()
+            .join("-")
     }
 
-    pub fn decode(&self, string: String) -> String {
-        // TODO: Decoding
-        String::from("Test")
+    pub fn decode(&self, string: &str) -> Option<String> {
+        let letters: Vec<u8> = string
+            .split("-")
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|&str| match str.parse::<usize>() {
+                Ok(_parsed) => Some(mod_exp::mod_exp(
+                    str.parse::<usize>().unwrap(),
+                    self.private_part,
+                    self.n,
+                ) as u8),
+                Err(error) => {
+                    println!("Could not parse input {:?}", error);
+                    None
+                }
+            })
+            .flatten()
+            .collect();
+
+        String::from_utf8(letters).ok()
     }
 
     fn generate_rsa() -> (usize, usize, usize) {
@@ -73,11 +95,32 @@ impl Rsa {
         let euler: usize = (first_prime - 1) * (second_prime - 1);
 
         // Get mutually prime number with euler's function in range [euler; euler * 2]
-        let d: usize = MutuallyPrimes::new(euler, euler * 2).choose(&mut rng).unwrap();
+        let d: usize = MutuallyPrimes::new(euler, euler * 2)
+            .choose(&mut rng)
+            .unwrap();
 
         // Get a number matching condition: (e * d) % euler == 1
         let e = NumberE::new(euler, d).next().unwrap();
 
         (e, d, n)
+    }
+}
+
+mod tests {
+    use crate::rsa::Rsa;
+
+    #[test]
+    fn ensure_integrity() {
+        let rsa: Rsa = Rsa::new();
+        let messages: [&str; 3] = [
+            "The quick brown fox jumps over the lazy dog",
+            "Test123",
+            "!@#$%^&",
+        ];
+
+        for &message in &messages {
+            let encoded: String = rsa.encode(message);
+            assert_eq!(rsa.decode(encoded.as_str()), Some(String::from(message)))
+        }
     }
 }
