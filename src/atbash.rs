598b8dfbf434985fa918221bc266349357606b9c
crimsonwrench <garon312@gmail.com>
2020-03-22 02:42:47 +0300

    Optimize & simplify code for atbash algorithm, update tests

--- src/atbash.rs
+++ src/atbash.rs
@@ -1,44 +1,61 @@
+fn replace(ch: &char, alphabet: &String, alphabet_size: &usize) -> char {
+    match alphabet
+        .chars()
+        .position(|alphabet_ch: char| alphabet_ch == *ch)
+    {
+        Some(index) => alphabet.chars().nth(alphabet_size - index - 1).unwrap(),
+        None => *ch,
+    }
+}
+
 pub fn encode(alphabet: &str, message: &str) -> String {
+    let alphabet_uppercase: String = alphabet.to_uppercase();
+    let alphabet_lowercase: String = alphabet.to_lowercase();
+    let alphabet_size: usize = alphabet.chars().count();
+
     message
         .chars()
-        .map(|ch| {
-            return match alphabet
-                .chars()
-                .position(|alphabet_ch: char| ch.to_uppercase().next().unwrap() == alphabet_ch)
-            {
-                Some(index) => {
-                    let result: char = alphabet
-                        .chars()
-                        .nth(alphabet.chars().count() - index - 1)
-                        .unwrap();
-
-                    if ch.is_lowercase() {
-                        return Some(result.to_lowercase().next().unwrap());
-                    }
-
-                    Some(result)
-                }
-                None => Some(ch),
-            };
+        .map(|ch: char| match ch {
+            'A'..='Z' | 'А'..='Я' | 'Ё' => replace(&ch, &alphabet_uppercase, &alphabet_size),
+            'a'..='z' | 'а'..='я' | 'ё' => replace(&ch, &alphabet_lowercase, &alphabet_size),
+            _ => ch,
         })
-        .flatten()
         .collect()
 }
 
 mod tests {
     #[test]
-    fn test_encoding() {
-        use crate::atbash::encode;
+    fn atbash_symmetry() {
+        use crate::atbash;
+        use std::collections::HashMap;
 
-        [
+        let mut tests = HashMap::new();
+
+        tests.insert(
             "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+            vec![
+                "тест",
+                "ТЕСТ",
+                "Съешь ещё этих мягких французских булок, да выпей же чаю",
+            ],
+        );
+        tests.insert(
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        ]
-        .iter()
-        .for_each(|msg: &&str| {
-            let encoded_message: String = encode(msg, msg);
-            let decoded_message: String = encode(msg, encoded_message.as_str());
-            assert_eq!(decoded_message, String::from(*msg));
-        });
+            vec![
+                "test",
+                "TEST",
+                "The quick brown fox jumps over the lazy dog",
+            ],
+        );
+
+        for (alphabet, sentences) in tests {
+            sentences.iter().for_each(|sentence| {
+                let encoded_string: String = atbash::encode(alphabet, sentence);
+                assert_eq!(
+                    atbash::encode(alphabet, encoded_string.as_str()),
+                    String::from(*sentence)
+                );
+            })
+        }
     }
 }
