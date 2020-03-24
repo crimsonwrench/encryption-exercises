dd0dc708267afe26bbbca943353c357cfe84d7e1
crimsonwrench <garon312@gmail.com>
2020-03-25 02:35:25 +0300

    Add Caesar cipher, group symmetric ciphers, add tests

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-mod atbash;
+mod symmetric;
 mod primes;
 mod rsa;
 
 use crate::rsa::Rsa;
 use std::io;
+use crate::symmetric::Symmetric;
 
 fn main() {
-    println!("---Generating rsa keys---");
+    println!("Generating rsa keys...");
     let rsa: Rsa = Rsa::new();
     loop {
         println!("---Encryption exercises---");
         println!("(1) Encode input using RSA algorithm");
         println!("(2) Decode input using RSA algorithm");
-        println!("(3) Encode input using Atbash algorithm");
+        println!("(3) Encode input using Atbash cipher");
+        println!("(4) Encode input using Caesar cipher");
         println!("(0) Exit");
 
         let mut input = String::new();
@@ -30,6 +32,7 @@ fn main() {
             1 => encode_rsa(&rsa),
             2 => decode_rsa(&rsa),
             3 => encode_atbash(),
+            4 => encode_caesar(),
             0 => break,
             _ => {
                 println!("Unknown command!");
@@ -86,15 +89,53 @@ fn encode_atbash() {
         Err(_) => return,
     };
 
-    match command {
-        1 => println!(
-            "Encoded message: {}",
-            atbash::encode("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ", message.as_str())
-        ),
-        2 => println!(
-            "Encoded message: {}",
-            atbash::encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", message.as_str())
-        ),
-        _ => println!("Unknown command!"),
+    let alphabet: &str = match command {
+        1 => "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+        _ => "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     };
+
+    println!("Encoded message: {}", Symmetric::new(alphabet).atbash(&message));
+}
+
+fn encode_caesar() {
+    println!("Enter the message to be encoded");
+    let mut message = String::new();
+
+    io::stdin()
+        .read_line(&mut message)
+        .expect("Failed to read line");
+
+    println!("---Select alphabet---");
+    println!("(1) Cyrillic");
+    println!("(2) Latin");
+
+    let mut command = String::new();
+
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
+
+    let command: usize = match command.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+
+    let alphabet: &str = match command {
+        1 => "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+        _ => "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    };
+
+    println!("Enter shift");
+    let mut shift = String::new();
+
+    io::stdin()
+        .read_line(&mut shift)
+        .expect("Failed to read line");
+
+    let shift: i32 = match shift.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+
+    println!("Encoded message: {}", Symmetric::new(alphabet).caesar(&message, shift));
 }
---
+++ src/symmetric.rs
@@ -0,0 +1,114 @@
+pub struct Symmetric {
+    chars_lc: String,
+    chars_uc: String,
+    size: usize
+}
+
+impl Symmetric {
+    pub fn new(characters: &str) -> Self {
+        Symmetric {
+            chars_lc: String::from(characters).to_lowercase(),
+            chars_uc: String::from(characters).to_uppercase(),
+            size: characters.chars().count()
+        }
+    }
+
+    pub fn atbash(&self, message: &str) -> String {
+        message
+            .chars()
+            .map(|ch: char| {
+                let alphabet_case: &String = self.get_case(&ch);
+                match alphabet_case.chars().position(|alphabet_ch: char| alphabet_ch == ch) {
+                    Some(position) => alphabet_case.chars().nth(self.size - position - 1).unwrap(),
+                    None => ch
+                }
+            }).collect()
+    }
+
+    pub fn caesar(&self, message: &str, shift: i32) -> String {
+        message
+            .chars()
+            .map(|ch: char| {
+                let alphabet_case: &String = self.get_case(&ch);
+                match alphabet_case.chars().position(|alphabet_ch: char| alphabet_ch == ch) {
+                    Some(position) => {
+                        return alphabet_case.chars().nth(((position as i32 + shift).rem_euclid(self.size as i32)) as usize).unwrap();
+                    },
+                    None => ch
+                }
+            }).collect()
+    }
+
+    fn get_case(&self, char: &char) -> &String {
+        if char.is_lowercase() {
+            return &self.chars_lc;
+        }
+
+        &self.chars_uc
+    }
+}
+
+mod tests {
+
+    #[test]
+    fn atbash_symmetry() {
+        use crate::symmetric::Symmetric;
+        use std::collections::HashMap;
+
+        let mut tests = HashMap::new();
+
+        tests.insert(
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+            vec![
+                "тест",
+                "ТЕСТ",
+                "Съешь ещё этих мягких французских булок, да выпей же чаю",
+            ],
+        );
+        tests.insert(
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            vec![
+                "test",
+                "TEST",
+                "The quick brown fox jumps over the lazy dog",
+            ],
+        );
+
+        for (alphabet, sentences) in tests {
+            let cipher = Symmetric::new(alphabet);
+            sentences.iter().for_each(|sentence| {
+                let encoded_string: String = cipher.atbash(sentence);
+                assert_eq!(
+                    cipher.atbash(&encoded_string),
+                    String::from(*sentence)
+                );
+            })
+        }
+    }
+
+    #[test]
+    fn caesar_symmetry() {
+        use crate::symmetric::Symmetric;
+        use rand::thread_rng;
+        use rand::Rng;
+
+        let latin_alphabet: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let random_shift: i32 = thread_rng().gen_range(-1 * latin_alphabet.len() as i32, latin_alphabet.len() as i32);
+
+        let cipher = Symmetric::new(latin_alphabet);
+
+
+        vec![
+            "The quick brown fox jumps over the lazy dog",
+            "test",
+            "TEST"
+        ].iter()
+            .for_each(|sentence| {
+                let encoded_sentence: String = cipher.caesar(*sentence, random_shift);
+                assert_eq!(
+                    cipher.caesar(&encoded_sentence, -1 * random_shift),
+                    String::from(*sentence)
+                )
+            })
+    }
+}
